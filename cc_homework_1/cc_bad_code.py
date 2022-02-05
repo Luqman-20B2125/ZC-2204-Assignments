b90b185f3c63567458fdf99822a888a104a68c70
Luqman/20b2125 <20b2125@ube.edu.bn>
2022-02-05 22:22:19 +0800

    changed cc_bad_code.py and cc_good_code.py

--- cc_homework_1/cc_bad_code.py
+++ cc_homework_1/cc_bad_code.py
@@ -1,1 +1,53 @@
+n = "Luqman" # Name is not informative, only a single letter so need to mentally map its meaning every time we see it
 
+flagBool = 0 # Name spreads disinformation
+
+l, O = 0, 0 # Using l or O for variable names as they look like 1 and 0 respectively, so might confuse people reviewing the code.
+if l == 0: 
+    print("1 is", O)
+
+schoolString = "Primary" # Different names but same implied meaning and redundant naming
+schoolNameString = "Stella School"
+
+fName, FName = "John", "John Smith"  # Similar names that does not distinguish the content of the variables.
+
+fMLN = "John Devan Smith"# Unpronouncable name
+
+dtOfExm = "22/3/2022" # Unsearchable name
+
+iMoneyAmount = 1000 # Encoding the name to indicate variable type, in this example we use i to indicate integer
+
+class Apply: # Naming a class using a verb
+    def __init__(self, data = None):
+        self.data = data
+
+def Book(): # Naming a function using a noun
+    print("Book")
+
+def TrumanShow(): # Naming a function using a cultural reference rather than its direct use
+    print("Hello World!")
+
+def AddTwoIntegers(int1, int2): # Using different words for the same effect on different functions or classes
+    m_sum = int1 + int2         # Using prefix to indicate variable being a member of a function or class
+    return m_sum
+
+def SumTwoFloats(float1, float2):
+    m_sum = float1 + float2
+    return m_sum
+
+itemCatalogue = ["Banana","Apple","Coconut"] # Using names that could instead use programming technical terms
+
+city, country = "Bandar Seri Begawan", "Brunei Darussalam" # Using names that do not have the context needed to give meaning
+
+def checkBool(flag): # functions that do multiple jobs and those with boolean as an argument,
+                     # as boolean means it will do multiple jobs
+    if flag == True:
+        print("Flag is True")
+    else:
+        print("Flag is False")
+checkBool(True)
+
+def printRepeat(string, numberOfRepetition):# Function with two arguments with no indication of argument order
+    for i in range(numberOfRepetition):
+        print(string)
+printRepeat("Hello World",3)
--- cc_homework_1/cc_clean_code.py
+++ cc_homework_1/cc_clean_code.py
@@ -1,1 +1,61 @@
+name = "Luqman"  # Name is now informative as we now know that the variable contains a name
+                 # and no need to mentally map what the variable is supposed to be.
 
+flagBool = True  # Name is now true, as variable now contains a boolean value
+
+zero, one = 0, 0 # Not using L or O, instead use a different variable that does not look similar
+if zero == 0: 
+    print("1 is", one)
+
+schoolType = "Primary"       # Now names have different meaning and indicates the content 
+schoolName = "Stella School" # and no longer redundant as schoolType and schoolName can only be strings.
+
+firstName, fullName = "John", "John Smith"  # Now distinguishes the two variables and their contents
+
+firstMiddleLastName = "John Devan Smith" # Name is now pronounceable and tells us what it contains
+
+dateOfExam = "22/3/2022" # Name is now searchable by entering date or exam
+
+moneyAmount = 1000 # No need to encode the variable as moneyAmount can only be a number
+
+class Application: # Change class name to a noun 
+    def __init__(self, data = None):
+        self.data = data
+
+def printBook(): # Add or use a verb instead to indicate what the function does
+    print("Book")
+
+def printHello(): # Don't use cultural reference, instead directly mention what function does
+    print("Hello World!")
+
+def SumTwoIntegers(int1, int2): # Use same word for the same effect on different functions or classes
+    sum = int1 + int2           # Remove the prefix as no need to indicate variable is a member, as that info is not needed
+    return sum
+
+def SumTwoFloats(float1, float2):
+    sum = float1 + float2
+    return sum
+
+itemList = ["Banana","Apple","Coconut"] # Use programming technical terms if applicable instead of problem domain terms.
+
+homeCity, homeCountry = "Bandar Seri Begawan", "Brunei Darussalam" # Add context to the variable name to give it meaning
+
+def checkBool(flag): # Make the other jobs of the function be a smaller function to be called, 
+                     # so that the main function does not have multiple jobs.
+                     # This should also be done for functions with boolean argument.
+    if flag == True:
+        flagTrue()
+    else:
+        flagFalse()
+    
+def flagTrue():
+    print("Flag is True")
+
+def flagFalse():
+    print("Flag is False")
+checkBool(True)
+
+def printRepeatStringNumberOfRepetition(string, numberOfRepetition): # Add the argument order into the name of the function
+    for i in range(numberOfRepetition):
+        print(string)
+printRepeatStringNumberOfRepetition("Hello World",3)
