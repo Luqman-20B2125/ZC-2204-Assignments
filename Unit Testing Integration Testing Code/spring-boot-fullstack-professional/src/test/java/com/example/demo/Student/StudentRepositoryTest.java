32ee6b4f126d4cb59e54e48df2ab6b9e83cd16c9
Luqman-20B2125 <98514041+Luqman-20B2125@users.noreply.github.com>
2022-03-19 22:58:35 +0800

    Update StudentRepositoryTest.java

--- Unit Testing Integration Testing Code/spring-boot-fullstack-professional/src/test/java/com/example/demo/Student/StudentRepositoryTest.java
+++ Unit Testing Integration Testing Code/spring-boot-fullstack-professional/src/test/java/com/example/demo/Student/StudentRepositoryTest.java
@@ -2,6 +2,14 @@ package com.example.demo.Student;
 
 import java.beans.Transient;
 
+import com.example.demo.student.Student;
+import com.example.demo.student.StudentRepository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 @DataJpaTest
 public class StudentRepositoryTest {
     
@@ -16,7 +24,7 @@ public class StudentRepositoryTest {
     @Test
     void itShouldCheckIfStudentEmailExists() {
         // Given
-        String email = "jamila@gmail.com"
+        String email = "jamila@gmail.com";
         Student student = new Student("Jamila", email,Gender.FEMALE);
         underTest.save(student);
 
@@ -30,7 +38,7 @@ public class StudentRepositoryTest {
     @Test
     void itShouldCheckIfStudentEmailDoesNotExist() {
         // Given
-        String email = "jamila@gmail.com"
+        String email = "jamila@gmail.com";
 
         // When
         boolean expected = underTest.selectExistsEmail(email);
