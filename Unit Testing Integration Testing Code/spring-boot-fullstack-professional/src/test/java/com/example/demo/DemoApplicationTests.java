4c8865e86cf7d92a08786425245362b42d82e3b7
Luqman-20B2125 <98514041+Luqman-20B2125@users.noreply.github.com>
2022-03-19 22:06:25 +0800

    Update DemoApplicationTests.java

--- Unit Testing Integration Testing Code/spring-boot-fullstack-professional/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Unit Testing Integration Testing Code/spring-boot-fullstack-professional/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,9 +5,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+	Calculator underTest = new Calculator();
 
 	@Test
-	void contextLoads() {
-	}
+	void itShouldAddTwoNumbers() {
+		// Given
+		int numberOne = 20;
+		int numberTwo = 30;
 
+		// When
+		int result = underTest.add(numberOne, numberTwo);
+
+		// Then
+		int expected = 50;
+		assertThat(result).isEqualTo(expected);
+	}
+	
+	class Calculator {
+		int add(int a, int b) {
+			return a + b;
+		}
+	}
 }
